c8826d4530c595e008477e8c52912cf299538e8e
XXXXZR <zunrongxie@163.com>
2019-08-01 20:21:28 +0800

    create database

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 ## 学习Spring Boot 
 
 ## 资料
-[Spring 文档](https://spring.io/guides)  
+[Spring Guide文档](https://spring.io/guides)  
 [Bootstrap 文档](https://v3.bootcss.com )   
 [Github OAuth API Doc](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
-[OKHttp Doc](https://square.github.io/okhttp/)
+[OKHttp Doc](https://square.github.io/okhttp/)            
+[MySQL教程](https://www.runoob.com/mysql/mysql-tutorial.html)   
+[Spring 使用文档](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
  
  ## 工具
  [Visual Paradigm 画时序图](https://www.visual-paradigm.com/cn/)
@@ -13,3 +15,22 @@
  ## 快捷键
  alt + ins  快速生成set get方法    
  ctrl + E   切换到最近编辑的一个窗口
+ 
+ 
+ 
+ ## 脚本
+ ```sql
+ create table USER
+ (
+ 	ID int auto_increment,
+ 	ACCOUNT_ID VARCHAR(100),
+ 	NAME VARCHAR(50),
+ 	TOKEN CHAR,
+ 	GMT_CREATE BIGINT,
+ 	GMT_MODIFIED BIGINT,
+ 	constraint USER_PK
+ 		primary key (ID)
+ );
+```
+
+ 
--- src/main/java/com/zun/community/community/controller/AuthorizeController.java
+++ src/main/java/com/zun/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.zun.community.community.controller;
 
 import com.zun.community.community.dto.AccessTokenDTO;
 import com.zun.community.community.dto.GithubUser;
+import com.zun.community.community.mapper.UserMapper;
+import com.zun.community.community.model.User;
 import com.zun.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -17,16 +22,18 @@ public class AuthorizeController {
 
     @Value("${github.client.id}")
     private String clientId;
-
     @Value("${github.client.secret}")
     private String clientSecret;
-
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,9 +41,23 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            //登录成功 写cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
+
     }
 
 
